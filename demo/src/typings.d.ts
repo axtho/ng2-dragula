c3cc1b6034809481c9126d856c62347cf71fd2a9
axtho <thomas.hochstetter@me.com>
2017-05-16 23:40:49 +0200

    upgrade deps

--- demo/src/typings.d.ts
+++ demo/src/typings.d.ts
@@ -6,5 +6,5 @@ declare const ENV:string;
 // google code-prettify
 declare const PR:any;
 
-declare const require:any;
-declare const global:any;
+// declare const require:any;
+// declare const global:any;
